aac29ffb5c1cfe2d2502360741b60efc21da2b47
Hosung Jin <42510802+jinhosung96@users.noreply.github.com>
2021-07-25 23:37:51 +0900

    feat: 각성 탭 구조 설계
    what?
    
    EvolutionSlot 구현

---
+++ Assets/# MyAsset/Scripts/EvolutionSlot/EvolutionSlot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JHS
+{
+    #region 머리말 주석
+    /// <summary>
+    ///
+    /// 원 저작자(개발자) : 진호성 <para></para>
+    /// 개요 : <para></para>
+    /// 
+    /// </summary>
+    #endregion
+    public class EvolutionSlot : MonoBehaviour
+    {
+        #region 필드
+
+        [SerializeField] Image icon;
+
+        #endregion
+
+        #region 속성
+
+    
+
+        #endregion
+
+        #region 유니티 생명주기
+
+    
+
+        #endregion
+
+        #region 공개 메소드
+
+    
+
+        #endregion
+
+        #region 내부 메소드
+
+    
+
+        #endregion
+    }
+}
