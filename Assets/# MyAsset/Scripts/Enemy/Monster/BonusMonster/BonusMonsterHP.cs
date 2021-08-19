c6c518b8e7162402eb0eb1d627bbbb1f3f15fb7d
Hosung Jin <42510802+jinhosung96@users.noreply.github.com>
2021-08-19 23:21:43 +0900

    feat: 보너스 몬스터 처치 시 보상 골드 증가
    what?
    
    1. MonsterHP를 상속받은 BonusMonsterHP 작성
    
    2. BonusMonster 보상 골드 두배 증가

---
+++ Assets/# MyAsset/Scripts/Enemy/Monster/BonusMonster/BonusMonsterHP.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+namespace JHS
+{
+    #region 머리말 주석
+    /// <summary>
+    ///
+    /// 원 저작자(개발자) : 진호성 <para></para>
+    /// 개요 : <para></para>
+    /// 
+    /// </summary>
+    #endregion
+    public class BonusMonsterHP : MonsterHP
+    {
+        #region 재정의 메소드
+
+        protected override BigInteger GetRoundHP() => StageSystem.Instance.RoundHP * 2;
+
+        protected override BigInteger GetRoundGold() => StageSystem.Instance.RoundGold * 2;
+
+        #endregion
+    }
+}
